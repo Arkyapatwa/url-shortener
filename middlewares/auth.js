60c3205fb28a1e068314e4cef4abc5026907f676
Arkya Patwa <arkyapatwa333@gmail.com>
2024-08-19 22:27:58 +0530

    static routes correction

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,13 +5,13 @@ async function restrictToLoginUsersOnly(req, res, next) {
     const session = req.cookies?.session
 
     if (!session) {
-        return res.redirect('/user/login')
+        return res.redirect('/login')
     }
 
     const user = getUser(session)
 
     if (!user) {
-        return res.redirect('/user/login')
+        return res.redirect('/login')
     }
 
     req.user = user
